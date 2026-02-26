f00914760cc841deda9bc98263daddab053472a4
Aarthika <arththika04@gmail.com>
2026-02-26 16:05:44 +0530

    first commit

---
+++ Backend/controllers/authController.js
@@ -0,0 +1,61 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import generateToken from "../utils/generateToken.js";
+
+export const register = async (req, res) => {
+  const { username, email, password, confirmPassword } = req.body;
+
+  if (!username || !email || !password || !confirmPassword) {
+    return res.status(400).json({ message: "All fields required" });
+  }
+
+  if (password !== confirmPassword) {
+    return res.status(400).json({ message: "Passwords do not match" });
+  }
+
+  if (password.length < 8) {
+    return res.status(400).json({ message: "Password must be 8+ chars" });
+  }
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return res.status(400).json({ message: "Email already exists" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  res.status(201).json({
+    message: "User registered successfully",
+  });
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+
+  if (!user.isActive) {
+    return res.status(403).json({ message: "Account deactivated" });
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return res.status(400).json({ message: "Invalid credentials" });
+  }
+
+  const token = generateToken(user);
+
+  res.json({
+    token,
+    role: user.role,
+  });
+};
---
+++ Backend/models/User.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      minlength: 3,
+      maxlength: 20,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+    },
+    googleId: {
+      type: String,
+    },
+    role: {
+      type: String,
+      enum: ["user", "dietician", "kitchen", "admin"],
+      default: "user",
+    },
+    isVerified: {
+      type: Boolean,
+      default: true,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
