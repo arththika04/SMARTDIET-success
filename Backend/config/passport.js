b50256c87d7ba6a07f28edaf096909ff7e804680
Aarthika <arththika04@gmail.com>
2026-02-27 19:25:52 +0530

    Add

---
+++ Backend/config/passport.js
@@ -0,0 +1,55 @@
+// Backend/config/passport.js
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import User from "../models/User.js";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const email = profile.emails[0].value;
+
+        let user = await User.findOne({ email });
+
+        if (!user) {
+          user = await User.create({
+            username:
+              profile.displayName ||
+              profile.name?.givenName ||
+              "google_user",
+            email,
+            googleId: profile.id,
+            isVerified: true,
+          });
+        } else if (!user.googleId) {
+          user.googleId = profile.id;
+          await user.save();
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err, null);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
+
+export default passport;
