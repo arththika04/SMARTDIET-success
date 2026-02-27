0bb508aea9fc704abd4a2801c56ee9dba22936dc
Aarthika <arththika04@gmail.com>
2026-02-27 11:57:20 +0530

    Add login feature

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,10 +1,18 @@
 import express from "express";
-import { register, login, logout } from "../controllers/authController.js";
+import {
+  register,
+  login,
+  logout,
+  forgotPassword,
+  resetPassword,
+} from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.post("/logout", logout);   // Logout route
+router.post("/logout", logout);
+router.post("/forgot-password", forgotPassword); // 🔹 new
+router.post("/reset-password", resetPassword);   // 🔹 new
 
 export default router;
